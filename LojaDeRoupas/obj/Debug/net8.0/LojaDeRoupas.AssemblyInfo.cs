04a03985a307e1725133f2e2f95ac68a60e9204f
Joao Nascimento <jhonefire@gmail.com>
2024-10-01 16:44:23 -0300

    deletado department and controller

--- LojaDeRoupas/obj/Debug/net8.0/LojaDeRoupas.AssemblyInfo.cs
+++ LojaDeRoupas/obj/Debug/net8.0/LojaDeRoupas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LojaDeRoupas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f1451289f071e72c46c67df576a96e652a36f5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d8aed076b8ae683a7a084e2e7f0f9d22c7f4249")]
 [assembly: System.Reflection.AssemblyProductAttribute("LojaDeRoupas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LojaDeRoupas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
